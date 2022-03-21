3264761738504590147c6dbb3bbd848e31e26a09
rafael1717y <rafael1717yy@gmail.com>
2022-03-21 08:55:43 -0300

    testes

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,7 +6,7 @@ def test_app_is_created(app):
 
 
 def test_config_is_loaded(config):
-    assert config["DEBUG"] is True
+    assert config["DEBUG"] is False
 
 
 def test_request_returns_404(client):
